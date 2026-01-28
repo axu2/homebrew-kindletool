671b9749c1bf607930942c1f679b9b86138b3bbc
W. Gustavo Cevallos <wgcv@users.noreply.github.com>
2026-01-27 22:59:14 -0500

    Update Kindletool to version 1.6.6
    Remove nettle

--- Formula/kindletool.rb
+++ Formula/kindletool.rb
@@ -1,21 +1,20 @@
 class Kindletool < Formula
   desc "Tool for creating/extracting Kindle updates and more"
   homepage "https://github.com/NiLuJe/KindleTool"
-  url "https://github.com/NiLuJe/KindleTool/archive/v1.6.5.tar.gz"
-  sha256 "949cbbd3390a10cb86ebff870a3e00566dbef33630fddb2cbd5ff81f90fb4030"
+  url "https://github.com/NiLuJe/KindleTool/archive/v1.6.6.tar.gz"
+  sha256 "b8bb6ba7c135404e2d3c82fcf7b21e2c460593d80dcf415486a20fde108c1667"
 
   head "https://github.com/NiLuJe/KindleTool.git", shallow: false
 
   depends_on "pkg-config" => :build
-
   depends_on "NiLuJe/kindletool/libarchive-git"
-  depends_on "NiLuJe/kindletool/nettle-git"
+  depends_on "nettle"
 
   def install
-    # Make sure the buildsystem will be able to generate a proper version tag
     ENV["GIT_DIR"] = cached_download/".git" if build.head?
 
     system "make"
     system "make", "install", "DESTDIR=#{prefix}", "PREFIX=/."
   end
 end
+
