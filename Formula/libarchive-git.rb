e3364d3f59c929baa3133e019adadf212a20b9ce
NiLuJe <ninuje@gmail.com>
2026-01-28 12:34:48 +0100

    Resync libarchive-git w/ upstream

--- Formula/libarchive-git.rb
+++ Formula/libarchive-git.rb
@@ -1,23 +1,24 @@
 class LibarchiveGit < Formula
   desc "Multi-format archive and compression library"
   homepage "https://www.libarchive.org"
-  url "https://www.libarchive.org/downloads/libarchive-3.5.1.tar.xz"
-  sha256 "0e17d3a8d0b206018693b27f08029b598f6ef03600c2b5d10c94ce58692e299b"
+  url "https://www.libarchive.org/downloads/libarchive-3.8.5.tar.xz"
+  sha256 "d68068e74beee3a0ec0dd04aee9037d5757fcc651591a6dcf1b6d542fb15a703"
   license "BSD-2-Clause"
 
   head "https://github.com/libarchive/libarchive.git"
 
   livecheck do
-    url "https://libarchive.org/downloads/"
+    url :homepage
     regex(/href=.*?libarchive[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "70d00c2b7edaa2e7e84d8831edddfc07490f19016f7899bcde581d5d71040d67"
-    sha256 cellar: :any, big_sur:       "88826e185e60eba60ad004889a76dae4354a15533a5ba8254ecf0323075b34cd"
-    sha256 cellar: :any, catalina:      "616792d4660cc153ce5868fd612980af9b8b9e18d54c4199eb11e08bc9bb45da"
-    sha256 cellar: :any, mojave:        "b231516d40d35180e1f61a50175324e4ce28f71aec27769ecc4661dbe6e883d0"
+    sha256 cellar: :any,                 arm64_tahoe:   "64462599d1d89aa6eb98797ca80c39bc491c1d3c7cafd4e1d76e8cca9d964100"
+    sha256 cellar: :any,                 arm64_sequoia: "e66ef5adf6e4a18e85b539843551353b53fb0c4889bc47797d721e6b36be50dd"
+    sha256 cellar: :any,                 arm64_sonoma:  "ad74cac45f900f21ca0a0449d5bcf2e8d5fe28ce7c11c126d386f372e2bb81d1"
+    sha256 cellar: :any,                 sonoma:        "4ea4029b386797ed0db92d5036f6b7dff9b39db852fcd1d690a144d9fbb5eb22"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9734647635e4b46166ed614de642a34838ce5d647e7a97bbc9345c3c972bac10"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4a5af0d3d3888ac0e912536969d456ab385c9fd914e6d56afda04db79ffddbd3"
   end
 
   keg_only :provided_by_macos
@@ -35,15 +36,9 @@ class LibarchiveGit < Formula
   uses_from_macos "expat"
   uses_from_macos "zlib"
 
-  on_linux do
-    conflicts_with "cpio", because: "both install `cpio` binaries"
-    conflicts_with "gnu-tar", because: "both install `tar` binaries"
-  end
-
   def install
     system "./build/autogen.sh" if build.head?
-    system "./configure",
-           "--prefix=#{prefix}",
+    system "./configure", *std_configure_args,
            "--without-lzo2",    # Use lzop binary instead of lzo2 due to GPL
            "--without-nettle",  # xar hashing option but GPLv3
            "--without-xml2",    # xar hashing option but tricky dependencies
@@ -51,6 +46,11 @@ class LibarchiveGit < Formula
            "--with-expat"       # best xar hashing option
 
     system "make", "install"
+
+    # Avoid hardcoding Cellar paths in dependents.
+    inreplace lib/"pkgconfig/libarchive.pc", prefix.to_s, opt_prefix.to_s
+
+    return unless OS.mac?
 
     # Just as apple does it.
     ln_s bin/"bsdtar", bin/"tar"
@@ -62,6 +62,6 @@ class LibarchiveGit < Formula
   test do
     (testpath/"test").write("test")
     system bin/"bsdtar", "-czvf", "test.tar.gz", "test"
-    assert_match(/test/, shell_output("#{bin}/bsdtar -xOzf test.tar.gz"))
+    assert_match "test", shell_output("#{bin}/bsdtar -xOzf test.tar.gz")
   end
 end
